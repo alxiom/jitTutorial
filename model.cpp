3b8f91b8b5b4530753c7434d8a25a7d095413bdc
alexkim <hyoungseok.k@gmail.com>
2019-01-14 00:15:53 +0900

    [Python] initial commit

---
+++ model.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <memory>
+#include <torch/script.h>
+
+int main(int argc, const char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "usage: model <input-vector>\n";
+        return -1;
+    }
+
+    std::shared_ptr<torch::jit::script::Module> module = torch::jit::load("../traced_model.pth");
+
+    std::vector<torch::jit::IValue> inputs;
+    inputs.push_back(torch::ones({1, 3}));
+
+    at::Tensor output = module->forward(inputs).toTensor();
+    std::cout << output.slice(/*dim=*/1, /*start=*/0, /*end=*/5) << '\n';
+}
---
+++ model.py
@@ -0,0 +1,28 @@
+import torch
+from torch import nn
+import numpy as np
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+
+        self.linear = nn.Sequential(
+            nn.Linear(in_features=3, out_features=100),
+            nn.Linear(in_features=100, out_features=5)
+        )
+
+    def forward(self, inputs):
+        return self.linear(inputs)
+
+
+if __name__ == "__main__":
+    model = Net()
+    tracer = torch.Tensor(np.random.random((1, 3))).float()
+    traced_net = torch.jit.trace(model, tracer)
+
+    print(model(tracer))
+    print(traced_net(tracer))
+
+    traced_net.save("traced_model.pth")
