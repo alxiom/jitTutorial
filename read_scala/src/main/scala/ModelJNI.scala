b0298801571650c938bd442bf02867d31e373483
alex <hyoungseok.k@gmail.com>
2019-01-23 23:29:17 +0900

    [Scala] load so file

--- read_scala/src/main/scala/ModelJNI.scala
+++ read_scala/src/main/scala/ModelJNI.scala
@@ -4,16 +4,22 @@
 
 class ModelJNI {
   // --- Native methods
-  @native def floatArrayMethod(a: Array[Float]):Array[Float]
+  @native def floatArrayMethod(x: Array[Float]):Array[Float]
+
 }
 
 // --- Code in App body will get wrapped in a main method on compilation
 object ModelJNI extends App {
 
   // --- Main method to test our native library
-  System.loadLibrary("ModelJNI")
-  val model = new ModelJNI
-  val prediction = model.floatArrayMethod(Array(1.0F, 1.0F, 1.0F))
 
-  println(s"floatArrayMethod: $prediction")
+//  System.setProperty("java.library.path", "/Users/alex/git/jitTutorial/read_scala/lib")
+//  System.loadLibrary("ModelJNI")
+
+  System.load("/Users/alex/git/jitTutorial/read_scala/lib/libModelJNI.so")
+
+  val model = new ModelJNI
+  val prediction = model.floatArrayMethod(Array(1.0F, 1.3F, 1.0F))
+
+  println(s"floatArrayMethod: ${prediction.mkString(",")}")
 }
