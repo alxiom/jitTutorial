d65100b84a2ba01f5ce968476131b2f2bb51a274
alexkim <hyoungseok.k@gmail.com>
2019-01-27 22:05:57 +0900

    [Python] change tracing example

--- train_python/trace.py
+++ train_python/trace.py
@@ -14,7 +14,9 @@ class Trace:
         output_dim = config.output_dim
 
         load_net = model.Net(data_dim, hidden_dim, output_dim, 1)
-        tracer = torch.Tensor(np.ones((1, seq_length, data_dim))).float()
+        load_net.load_state_dict(torch.load("save_model.pth"))
+        tracer = torch.Tensor(np.arange(1, seq_length * data_dim + 1).reshape((1, seq_length, data_dim))).float()
+        print(tracer)
         traced_net = torch.jit.trace(load_net, tracer)
 
         print(load_net(tracer))
