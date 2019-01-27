d428999e8d48a3d92e3d653bb272ecbf96eb4823
alexkim <hyoungseok.k@gmail.com>
2019-01-27 22:06:14 +0900

    [Cpp] enable sequence input

--- freeze_cpp/Model.cpp
+++ freeze_cpp/Model.cpp
@@ -2,8 +2,11 @@
 #include <torch/script.h>
 #include "Model.hpp"
 #include "EvalJNI.h"
-#define INPUT_SIZE 3
-#define OUTPUT_SIZE 5
+#define INPUT_BATCH_SIZE 1
+#define INPUT_SEQUENCE_SIZE 7
+#define INPUT_ELEMENT_SIZE 5
+#define OUTPUT_SIZE 1
+using namespace std;
 
 namespace pytorch {
     Model::Model () {}
@@ -11,14 +14,12 @@ namespace pytorch {
         this -> module = torch::jit::load(modelName);
     }
     Model::~Model () {}
-    std::vector<float> Model::predict (std::vector<float> x) {
-        std::vector<std::vector<float>> xTensor;
-        xTensor.push_back(x);
-        at::Tensor inputVector = torch::from_blob(&xTensor[0][0], {1, INPUT_SIZE}, at::kFloat).clone();
-        std::vector<torch::jit::IValue> inputTensor;
+    vector<float> Model::predict (vector<float> x) {
+        at::Tensor inputVector = torch::from_blob(&x[0], {INPUT_BATCH_SIZE, INPUT_SEQUENCE_SIZE, INPUT_ELEMENT_SIZE}, at::kFloat).clone();
+        vector<torch::jit::IValue> inputTensor;
         inputTensor.push_back(inputVector);
         at::Tensor outputTensor = this -> module -> forward(inputTensor).toTensor();
-        std::vector<float> outputVector(outputTensor.data<float>(), outputTensor.data<float>() + outputTensor.numel());
+        vector<float> outputVector(outputTensor.data<float>(), outputTensor.data<float>() + outputTensor.numel());
         return outputVector;
     }
 
@@ -29,8 +30,8 @@ namespace pytorch {
         return long (pModel);
     }
     float* Eval::evaluate(long pModel, float* x) {
-        std::vector<float> vectorX (x, x + INPUT_SIZE);
-        std::vector<float> vectorY = ((Model*)pModel) -> Model::predict(vectorX);
+        vector<float> vectorX (x, x + INPUT_BATCH_SIZE * INPUT_SEQUENCE_SIZE * INPUT_ELEMENT_SIZE);
+        vector<float> vectorY = ((Model*)pModel) -> Model::predict(vectorX);
         static float y[OUTPUT_SIZE];
         for (int i = 0; i < OUTPUT_SIZE; ++i) {
             y[i] = vectorY[i];
@@ -65,20 +66,25 @@ JNIEXPORT jfloatArray JNICALL Java_EvalJNI_evaluate
 
 int main(int argc, const char* argv[]) {
     if (argc != 2) {
-        std::cerr << "argument mismatched\n";
+        cerr << "argument mismatched\n";
         return -1;
     }
 
     pytorch::Model model = pytorch::Model("../../train_python/trace_model.pth");
-    std::vector<float> vectorX = {1.0, 1.0, 1.0};
-    std::vector<float> vectorY = model.predict(vectorX);
-    std::cout << "model prediction" << std::endl;
-    std::cout << vectorY << std::endl;
+    vector<float> vectorX;
+    for (int i = 0; i < INPUT_BATCH_SIZE * INPUT_SEQUENCE_SIZE * INPUT_ELEMENT_SIZE; ++i) {
+	vectorX.push_back(i + 1);
+    }
+    vector<float> vectorY = model.predict(vectorX);
+    cout << "model prediction" << endl;
+    cout << vectorY << endl;
 
     pytorch::Eval eval = pytorch::Eval();
     long loadModel = eval.loadModel("../../train_python/trace_model.pth");
     float* x = &vectorX[0];
     float* y = eval.evaluate(loadModel, x);
-    std::cout << "external evaluation" << std::endl;
-    std::cout << y[0] << " " << y[1] << " " << y[2] << " " << y[3] << " " << y[4] << std::endl;
+    cout << "external evaluation" << endl;
+    for (int i = 0; i < OUTPUT_SIZE; ++i) {
+    	cout << y[i] << " " << endl;
+    }
 }
